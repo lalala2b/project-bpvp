11d9580356090ff919a024908a46e2784e27be97
lalala <55229767+lalala2b@users.noreply.github.com>
2023-11-09 18:20:09 +0800

    Add files via upload

---
+++ ProjectESP32/Smart Home Project/main_system_v0.1/main_system_v0.1.ino
@@ -0,0 +1,90 @@
+// Library
+#include <DHT.h>
+
+#define DHTPin 27
+#define DHTType 11
+DHT dht(DHTPin, DHTType);
+
+// Magnet switch
+// #define magnetSw1 
+
+// Sensor Gas MQ2  SENSOR GAS MASIH BELUM MASUK
+//#define sensorMQ2 13
+
+// Buzzer pin
+#define buzzer 5
+
+// Lampu menggunakan LDR
+#define LDR 34
+const int sensorMin=0;
+const int sensorMax=1024;
+  
+// PIN Lampu
+#define lampu1 18
+#define lampu2 19
+
+// Sensor Api
+#define sensorApi 35
+
+void setup(){
+  Serial.begin(9600);
+
+  // lampu LDR
+  pinMode(lampu1,OUTPUT);
+  pinMode(lampu2,OUTPUT);
+  pinMode(buzzer,OUTPUT);
+  pinMode(sensorMQ2,INPUT);
+
+  // dht
+  dht.begin();
+}
+
+void loop(){
+  delay(1000);
+
+  // Sensor Api
+  int nilaiApi = analogRead(35);
+  Serial.println("Api: "+String(nilaiApi));
+  int range = map(nilaiApi, sensorMin, sensorMax, 0, 3);
+  Serial.println("Range: "+String(range));
+  Serial.println();
+  if (nilaiApi<2000);
+    digitalWrite(buzzer,HIGH);
+  }else {
+    digitalWrite(buzzer,LOW);
+  }
+
+
+  // Lampu menggunakan LDR
+  const float GAMMA = 0.7;
+  const float RL10 = 50;
+
+  float ldrVal = analogRead(LDR);
+  float voltage = ldrVal / 4096*5;
+  float resistance = 2000*voltage/(1-voltage/5);
+  float lux = pow(RL10*1e3*pow(18, GAMMA)/resistance, (1/GAMMA));
+  Serial.println(ldrVal);
+  Serial.println(voltage);
+  Serial.println(lux);
+  Serial.println();
+
+  if (ldrVal<200){ 
+    digitalWrite(lampu1,HIGH);
+    digitalWrite(lampu2,HIGH);
+  }else{
+    digitalWrite(lampu1,LOW);
+    digitalWrite(lampu2,LOW);
+  }
+  // Lampu menggunakan LDR
+
+  // Sensor DHT11
+  float temp = dht.readTemperature();
+  float hum = dht.readHumidity();
+
+  Serial.println("Temperature: "+String(temp)+"C");
+  Serial.println("Humidity: "+String(hum)+"%");
+  Serial.println();
+
+  // Sensor Gas MQ2
+  
+}
